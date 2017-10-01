41cd2f7565640028b6ccd923ad7e04f404586f5c
MendezBruno <brunomendez88@hotmail.com>
2017-10-01 19:04:46 -0300

    componentes principales creados, rutas principal creada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { UserCrudComponent } from './user-crud/user-crud.component';
+import { AdminUsersComponent} from './admin-users/admin-users.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/inicio', pathMatch: 'full'},
+  { path: 'inicio', component: InicioComponent},
+  { path: 'user/:id', component: UserCrudComponent},
+  { path: 'adminusers', component: AdminUsersComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    InicioComponent,
+    UserCrudComponent,
+    AdminUsersComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } //Para debug
+    ),
     BrowserModule
   ],
   providers: [],
